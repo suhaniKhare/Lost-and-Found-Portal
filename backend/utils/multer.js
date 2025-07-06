3515119ebb491fa0a747ec85c5f43a56a7af2fc9
suhaniKhare <suhanikhare1345@gmail.com>
2025-07-06 22:52:20 +0530

    multer destination change

--- backend/utils/multer.js
+++ backend/utils/multer.js
@@ -3,7 +3,8 @@ const path = require('path') ;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../public/image')); 
+    // cb(null, path.join(__dirname, '../public/image')); 
+    cb(null  , "/tmp") ;
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
